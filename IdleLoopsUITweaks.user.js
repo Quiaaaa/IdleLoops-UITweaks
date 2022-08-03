feb07e84927937d1579ae1686aa95d17c29df88e
Kitty <110419730+Demonkitzy@users.noreply.github.com>
2022-08-03 12:52:56 +0100

    Added cap button to haggle to set to 15

--- IdleLoopsUITweaks.user.js
+++ IdleLoopsUITweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quia's IdleLoops UI Mods
 // @namespace    https://github.com/Quiaaaa/
-// @version      0.4.6
+// @version      0.4.7
 // @description  Add some QoL UI elements for observing progress, and planning
 // @downloadURL  https://raw.githubusercontent.com/Quiaaaa/IdleLoops-UITweaks/main/IdleLoopsUITweaks.user.js
 // @author       Trimpscord
