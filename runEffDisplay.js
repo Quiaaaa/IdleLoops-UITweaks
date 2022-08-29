4945ff7f215f3fa435fad40787c498b08e0f8776
Quiaaaa <96918270+Quiaaaa@users.noreply.github.com>
2022-08-28 18:25:17 -0600

    runEff: added sorting options

--- runEffDisplay.js
+++ runEffDisplay.js
@@ -1,13 +1,14 @@
-window.displayManaEff = () => {
+window.displayManaEff = (sortBy="order") => {
 	// Console display of the mana/time efficiencies of all used mana/gold gain actions.
+	// Sort by options: "order" "mana" "eff" "time"
 	let manaPerGold = Math.floor(50 * getSkillBonus("Mercantilism"));
 	let previous = Koviko.cache.cache[0].data[0].resources;
 	let actions = {
-		"Sell Potions": {time: 0, mana: 0, gold: 0, order: 0}, 
-		"Sell Artifact": {time: 0, mana: 0, gold: 0, order: 0},
-		"Adventure Guild": {time: 0, mana: 0, gold: 0, order: 0},
-		"Mana Geyser": {time: 0, mana: 0, gold: 0, order: 0},
-		"Collect Taxes": {time: 0, mana: 0, gold: 0, order: 0},
+		"Sell Potions": {time: 0, mana: 0, gold: 0, order: 0, eff: 0}, 
+		"Sell Artifact": {time: 0, mana: 0, gold: 0, order: 0, eff: 0},
+		"Adventure Guild": {time: 0, mana: 0, gold: 0, order: 0, eff: 0},
+		"Mana Geyser": {time: 0, mana: 0, gold: 0, order: 0, eff: 0},
+		"Collect Taxes": {time: 0, mana: 0, gold: 0, order: 0, eff: 0},
 	};	
 	for (pred of Koviko.cache.cache) {
 		let name = pred.key[0];
@@ -43,24 +44,25 @@ window.displayManaEff = () => {
 			if (name?.includes("Buy Mana")) break;
 			
 			if (delta > 0) {
-				if (!(name in actions)) { actions[name] = {time: 0, mana: 0}; }
+				if (!(name in actions)) { actions[name] = {time: 0, mana: 0, gold: 0, order: 0, eff: 0}; }
 				actions[name].time += current.actionTicks;
 				actions[name].mana += delta;
 				actions[name].order = current.totalTicks;
+				actions[name].eff   = actions[name].mana / (actions[name].time/50)
 			}
 		}
 		previous = current;
 	}
 	
 	// Sort and display in console
-	let outStr = "Mana\tSec\tMana/s\tAction\n"
-	let sorted = Object.entries(actions).sort((a, b) => a[1].order - b[1].order);
+	let outStr = "Mana\tSec \tMana/s\tAction\n"
+	let sorted = Object.entries(actions).sort((a, b) => a[1][sortBy] - b[1][sortBy]);
 	for (const [name, data] of sorted) {
 		if (data.time <= 0 || data.mana <= 0) continue;
 		let seconds = data.time/50
-		outStr += `${intToString(data.mana,1)} \t${intToString(seconds,2)} \t${intToString(data.mana/seconds,1)}\t${name}\n`
+		outStr += `${intToString(data.mana,1)} \t${intToString(seconds,2)} \t${intToString(data.eff,1)}\t${name}\n`
 	}
 	console.log(outStr)
 }
 
-displayManaEff()
+displayManaEff() 
