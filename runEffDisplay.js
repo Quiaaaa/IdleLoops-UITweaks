5609d2ab3c997b29a42a510f1c88cf8bc3e16ca5
Quiaaaa <96918270+Quiaaaa@users.noreply.github.com>
2022-09-02 01:13:12 -0600

    RunEff: add total gains display

--- runEffDisplay.js
+++ runEffDisplay.js
@@ -3,6 +3,7 @@ window.displayManaEff = (sortBy="order") => {
 	// Sort by options: "order" "mana" "eff" "time"
 	let manaPerGold = Math.floor(50 * getSkillBonus("Mercantilism"));
 	let previous = Koviko.cache.cache[0].data[0].resources;
+	let totals = {"Total Gold": 0, "Gold to Mana": 0, "Raw Mana": 0, "Bought Mana": 0};
 	let actions = {
 		"Sell Potions": {time: 0, mana: 0, gold: 0, order: 0, eff: 0}, 
 		"Sell Artifact": {time: 0, mana: 0, gold: 0, order: 0, eff: 0},
@@ -11,6 +12,7 @@ window.displayManaEff = (sortBy="order") => {
 		"Collect Taxes": {time: 0, mana: 0, gold: 0, order: 0, eff: 0},
 	};	
 	for (pred of Koviko.cache.cache) {
+		if (pred.key[2]) continue; // disabled actions
 		let name = pred.key[0];
 		let current = pred.data[0].resources;
 		
@@ -38,10 +40,29 @@ window.displayManaEff = (sortBy="order") => {
 		
 		for (resource of ["mana", "gold"]) {
 			delta = current[resource] - previous[resource];
-			if (resource === "gold") { delta *= manaPerGold; } 
-			else if (delta > 0) { delta += current.actionTicks/100 * getSpeedMult(current.town) * 50; }
-			if (actions[name]?.gold > 0) { delta += actions[name].gold * manaPerGold; }
-			if (name?.includes("Buy Mana")) break;
+			if (resource === "gold") { 
+				if (delta > 0) {
+					totals["Total Gold"] += delta;
+				}
+				delta *= manaPerGold; 
+			} 
+			else if (delta > 0) { 
+				// adjust for the mana cost of the action itself
+				delta += current.actionTicks/100 * getSpeedMult(current.town) * 50; 
+				if (name?.includes("Buy Mana")) {
+					totals["Bought Mana"] += delta;
+					totals["Gold to Mana"] += previous.gold;
+					break; // ignore buy mana actions in summary
+				}
+				else {
+					totals["Raw Mana"] += delta;
+				}
+			}
+			if (actions[name]?.gold > 0) { 
+				// adjust for prior gold cost of action (aka pickaxe)
+				delta += actions[name].gold * manaPerGold; 
+			}
+
 			
 			if (delta > 0) {
 				if (!(name in actions)) { actions[name] = {time: 0, mana: 0, gold: 0, order: 0, eff: 0}; }
@@ -62,6 +83,7 @@ window.displayManaEff = (sortBy="order") => {
 		let seconds = data.time/50
 		outStr += `${intToString(data.mana,1)} \t${intToString(seconds,2)} \t${intToString(data.eff,1)}\t${name}\n`
 	}
+	outStr += "\n" + Object.entries(totals).map((total) => total[0] + "\t" + intToString(total[1],1)).join("\n");
 	console.log(outStr)
 }
 
